4c726c6a6fa1726a233622049cc436bab7fd9a8b
Jan Schmitte <janschmitte@gmail.com>
2020-11-13 15:28:19 +0100

    frm_AndroidTree * optimized loading of the device's directory tree    * was a recursive function iterating through all directories adding the subdirectories to the tree using numerous shell calls    * now gets a directory listing in one go and iterates through that listing

--- AndroidSync/frm_AndroidTree.vb
+++ AndroidSync/frm_AndroidTree.vb
@@ -32,53 +32,79 @@
         Me.Cursor = Cursors.AppStarting
 
         rootNode = New TreeNode(device.Model)
+        rootNode.Name = device.Model
         rootNode.ImageIndex = 0
         rootNode.SelectedImageIndex = 0
+
         trv_AndroidTree.Nodes.Add(rootNode)
 
         str_Task.Text = "Populating TreeView..."
         Application.DoEvents()
 
-        ' getting root nodes
-        Dim sdcards() As String = device.executeCommand("ls -1d /sdcard*").Split(ControlChars.CrLf.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-
-        For Each sdcard In sdcards
-            sdcard = sdcard.Replace("/", "")
-
-            Dim newNode As New TreeNode(sdcard)
-            newNode.ImageIndex = 1
-            newNode.SelectedImageIndex = 1
-            rootNode.Nodes.Add(newNode)
-
-            addSubNodes(newNode.FullPath.Replace(device.Model, ""), newNode)
-        Next
+        populateTreeView()
 
         str_Task.Text = "Done"
         Me.Cursor = Cursors.Default
     End Sub
 
-    Private Sub addSubNodes(path As String, parentNode As TreeNode)
-        If (cancelOrder = True) Then Exit Sub
+    Private Sub populateTreeView()
+        ' ToDo: can this be moved into a backgroundworker to keep the UI responsive?
+        str_Task.Text = "Getting directory listing from device..."
+        Dim lines() As String = device.executeCommand("find /sdcard*/ -type d | sort").Split(ControlChars.CrLf.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
 
-        Dim subdirs As List(Of String) = device.getSubdirectories(path)
+        str_Task.Text = "Populating TreeView..."
+        For Each line In lines
+            If (cancelOrder = True) Then Exit Sub
 
-        Application.DoEvents()
+            line = System.Text.RegularExpressions.Regex.Replace(line, "/$", "")
 
-        For Each subdir In subdirs
-            subdir = subdir.Replace("\", "")
+            Dim directories() As String = line.Split({"/"}, StringSplitOptions.RemoveEmptyEntries)
 
-            Dim newNode As New TreeNode(subdir)
-            newNode.ImageIndex = 2
-            newNode.SelectedImageIndex = 2
+            Select Case UBound(directories)
+                Case = 0
+                    ' create new node
+                    Dim newNode As New TreeNode(directories(0))
+                    newNode.Name = line
+                    newNode.ImageIndex = 1
+                    newNode.SelectedImageIndex = 1
 
-            parentNode.Nodes.Add(newNode)
+                    ' add new node to ROOTNODE
+                    rootNode.Nodes.Add(newNode)
+                    rootNode.Expand()
+                    newNode.Expand()
+                Case Else
+                    ' create new node
+                    Dim newNode As New TreeNode(directories.Last)
+                    newNode.Name = line
+                    newNode.ImageIndex = 2
+                    newNode.SelectedImageIndex = 2
 
-            addSubNodes(parentNode.FullPath.Replace(device.Model, "") & "/" & subdir, newNode)
+                    ' get PARENTNODE to newly created node
+                    ' replace special REGEX chars in directory names
+                    Dim toBeReplaced As String = replaceSpecialChars(directories.Last)
+                    toBeReplaced = "/" & toBeReplaced & "$"
+                    Dim fullPathParent = System.Text.RegularExpressions.Regex.Replace(line, toBeReplaced, "")
+                    Dim parentNode As TreeNode = trv_AndroidTree.Nodes.Find(fullPathParent, True).First
+
+                    ' add new node to PARENTNODE
+                    parentNode.Nodes.Add(newNode)
+            End Select
+
+            Application.DoEvents()
         Next
     End Sub
 
+    Private Function replaceSpecialChars(input As String) As String
+        Dim specialCharacters As String = "()[].?^$+"
+
+        For Each c In specialCharacters
+            input = input.Replace(c, "\" & c)
+        Next
+
+        Return input
+    End Function
+
     Private Sub btn_OK_Click(sender As Object, e As EventArgs) Handles btn_OK.Click
-        ' ToDo: return selected path from TreeView
         Me.selectedPath = trv_AndroidTree.SelectedNode.FullPath.Replace(device.Model, "")
 
         If (Me.selectedPath = "") Then
